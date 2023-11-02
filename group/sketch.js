8f816042c5e0b425ca45d63058926b0d052fc096
xuanfanzhao <115526270+xuanfanzhao@users.noreply.github.com>
2023-11-02 12:23:01 +1100

    group final code

--- group/sketch.js
+++ group/sketch.js
@@ -1,29 +1,29 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(229, 228, 240); // 设置背景颜色
+  background(229, 228, 240); 
   
-  // 根据浏览器窗口大小重新计算位置
+ // Recalculate the location based on the browser window size
   let yPosArray = calculatePositions([10, 50, 120, 150, 220, 250, 280, 340, 440], windowHeight);
   let xPosArray = calculatePositions([10, 30, 70, 140, 300, 330, 420, 440, 480, 500], windowWidth);
   
   horizontalStreets(yPosArray);
   verticalStreets(xPosArray);
   
-  // 创建紫色方块
-  createBlock(windowWidth * 0.1, windowHeight * 0.16, windowWidth * 0.06, windowHeight * 0.06, color(150, 150, 220));
-  createBlock(windowWidth * 0.1, windowHeight * 0.7, windowWidth * 0.08, windowHeight * 0.08, color(150, 150, 220));
-  createBlock(windowWidth * 0.32, windowHeight * 0.52, windowWidth * 0.06, windowHeight * 0.1, color(150, 150, 220));
-  createBlock(windowWidth * 0.76, windowHeight * 0.32, windowWidth * 0.1, windowHeight * 0.2, color(150, 150, 220));
-  createBlock(windowWidth * 0.82, windowHeight * 0.7, windowWidth * 0.08, windowHeight * 0.08, color(150, 150, 220));
+// Create a blue square
+  createBlock(windowWidth * 0.1, windowHeight * 0.16, windowWidth * 0.06, windowHeight * 0.06, color(0, 0, 255));
+  createBlock(windowWidth * 0.1, windowHeight * 0.7, windowWidth * 0.08, windowHeight * 0.08, color(0, 0, 255));
+  createBlock(windowWidth * 0.32, windowHeight * 0.52, windowWidth * 0.06, windowHeight * 0.1, color(0, 0, 255));
+  createBlock(windowWidth * 0.76, windowHeight * 0.32, windowWidth * 0.1, windowHeight * 0.2, color(0, 0, 255));
+  createBlock(windowWidth * 0.82, windowHeight * 0.7, windowWidth * 0.08, windowHeight * 0.08, color(0, 0, 255));
   
-  // 创建红色方块
-  createBlock(windowWidth * 0.16, windowHeight * 0.04, windowWidth * 0.04, windowHeight * 0.18, color(255, 70, 70));
-  createBlock(windowWidth * 0.26, windowHeight * 0.04, windowWidth * 0.08, windowHeight * 0.12, color(255, 70, 70));
-  createBlock(windowWidth * 0.16, windowHeight * 0.54, windowWidth * 0.12, windowHeight * 0.08, color(2255, 70, 70));
-  createBlock(windowWidth * 0.58, windowHeight * 0.4, windowWidth * 0.12, windowHeight * 0.1, color(255, 70, 70));
-  createBlock(windowWidth * 0.68, windowHeight * 0.6, windowWidth * 0.1, windowHeight * 0.14, color(255, 70, 70));
+  // Create a red square
+  createBlock(windowWidth * 0.16, windowHeight * 0.04, windowWidth * 0.04, windowHeight * 0.18, color(255, 0, 0));
+  createBlock(windowWidth * 0.26, windowHeight * 0.04, windowWidth * 0.08, windowHeight * 0.12, color(255, 0, 0));
+  createBlock(windowWidth * 0.16, windowHeight * 0.54, windowWidth * 0.12, windowHeight * 0.08, color(255, 0, 0));
+  createBlock(windowWidth * 0.58, windowHeight * 0.4, windowWidth * 0.12, windowHeight * 0.1, color(255, 0, 0));
+  createBlock(windowWidth * 0.68, windowHeight * 0.6, windowWidth * 0.1, windowHeight * 0.14, color(255, 0, 0));
   
-  // 创建灰色方块
+  // Create a gray box
   createBlock(windowWidth * 0.28, windowHeight * 0.08, windowWidth * 0.06, windowHeight * 0.06, color(169));
   createBlock(windowWidth * 0.46, windowHeight * 0.22, windowWidth * 0.1, windowHeight * 0.14, color(169));
   createBlock(windowWidth * 0.46, windowHeight * 0.74, windowWidth * 0.06, windowHeight * 0.1, color(169));
@@ -32,7 +32,7 @@ function setup() {
 }
 
 function calculatePositions(positionArray, canvasSize) {
-  // 将原始位置按照新画布大小进行调整
+  // Adjust the original position to the new canvas size
   let adjustedPositions = [];
   for (let pos of positionArray) {
     adjustedPositions.push((pos / 500) * canvasSize);
@@ -57,31 +57,28 @@ function horizontalStreets(yPosArray) {
 
 function verticalStreets(xPosArray) {
   for (let xPos of xPosArray) {
-    for (let i = 0; i < height; i += 20) {
+    for (let i = 0; i < height; i += 10) {
       let num = floor(random(101));
       let c = colourMap(num);
-      createBlock(xPos, i, 20, 20, c);
+      createBlock(xPos, i, 10, 10, c);
     }
   }
 }
 
 function colourMap(num) {
   if (num >= 0 && num <= 65) {
-    return color(255, 255, 0); // green
+    return color(255, 255, 0); // Yellow
   } else if (num >= 66 && num <= 80) {
     return color(0); // Black
   } else if (num >= 81 && num <= 85) {
-    return color(255, 0, 0); // blue
+    return color(255, 0, 0); // Red
   } else if (num >= 86 && num <= 100) {
-    return color(255,0,255); // pink
-  }else if (num >= 76 && num <= 100) {
-    return color(255, 165, 0); // 橙色
-      
+    return color(169); // Gray
   }
 }
 
 function windowResized() {
-  // 处理浏览器窗口大小变化时的情况
+  // Handle when the browser window size changes
   resizeCanvas(windowWidth, windowHeight);
-  setup(); // 重新创建和绘制图形
+  setup(); // Recreate and draw the graph
 }
